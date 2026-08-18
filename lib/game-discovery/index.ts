361c455b5fe92603f19c94d5bcc4d0d7de7eeac0
battlestartvr-factory <battlestartvr@gmail.com>
2026-08-18 12:45:54 +0300

    Stage 4: export economy and visual planning modules

--- lib/game-discovery/index.ts
+++ lib/game-discovery/index.ts
@@ -1,2 +1,6 @@
 export * from "./schemas";
 export * from "./diversity";
+export * from "./model-policy";
+export * from "./shot-planner";
+export * from "./prompt-compiler";
+export * from "./feedback-memory";
